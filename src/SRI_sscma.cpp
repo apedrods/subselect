c7d2766bf5d04ab587943f917e94ba90ca5d5c8a
PedroDS <psilva@ucp.pt>
2025-03-16 11:49:26 +0000

    sscma tests 6

--- src/SRI_sscma.cpp
+++ src/SRI_sscma.cpp
@@ -29,19 +29,26 @@ SEXP eleaps(SEXP S,SEXP S2,SEXP Si,SEXP Segval,SEXP Segvct,
    	Rf_protect(values = Rf_allocVector(REALSXP,nsol1*klength));
    	Rf_protect(bestsets = Rf_allocVector(INTSXP,kmax1*klength));
    	Rf_protect(bestvalues = Rf_allocVector(REALSXP,klength));
-
+/*
 	int retcode = extendedleaps::callsscma(
 		REAL(S),REAL(S2),REAL(Si),REAL(Segval),REAL(Segvct),
 		REAL(E),REAL(Ei),REAL(Hegvct),REAL(HegvctTinv),REAL(HegvctEinv),
 		REAL(wilksval)[0],REAL(bartpival)[0],REAL(lawhotval)[0],REAL(ccr12val)[0],
 		INTEGER(r)[0],kmin1,kmax1,nsol1,
 		INTEGER(exclude),INTEGER(include),INTEGER(nexclude)[0],INTEGER(ninclude)[0],
-/*		CHAR(STRING_ELT(criterion,0)), */ INTEGER(fixed)[0],INTEGER(pcindices),INTEGER(nbindices)[0],
+    CHAR(STRING_ELT(criterion,0)), INTEGER(fixed)[0],INTEGER(pcindices),INTEGER(nbindices)[0],
 		dim1,REAL(timelimit)[0],REAL(maxaperr)[0],checkcolinearity,
 		INTEGER(subsets),REAL(values),REAL(bestvalues),INTEGER(bestsets),
 		false
 	);
-
+*/
+	
+	int retcode = extendedleaps::callsscma(
+	  REAL(S),REAL(S2),REAL(Si),REAL(Segval),REAL(Segvct),
+	  REAL(E),REAL(Ei),REAL(Hegvct),REAL(HegvctTinv),REAL(HegvctEinv),
+    INTEGER(exclude),INTEGER(include),INTEGER(pcindices),
+    INTEGER(subsets),REAL(values),REAL(bestvalues),INTEGER(bestsets))
+	  
 //  int retcode = 0;
 
 	if (retcode == 4) nomemory = true;
--- src/SRI_sscma.h
+++ src/SRI_sscma.h
@@ -12,13 +12,18 @@ SEXP eleaps(SEXP S,SEXP S2,SEXP Si,SEXP Segval,SEXP Segvct,
 	SEXP dim,SEXP timelimit,SEXP ntol,SEXP onlyforward);
 
 namespace extendedleaps {
-		
+/*		
 int callsscma(double *S,double *S2,double *Si,double *Segval,double *Segvct,
 	double *E,double *Ei,double *Hegvct,double *HegvctTinv,double *HegvctEinv,
 	double wilksval,double bartpival,double lawhotval,double ccr12val,int r,
 	int kmin,int kmax,int nsol,int *out,int *in,int nout,int nin,
-/*	const char *cmpcr, */ int fixed,int *pcind,int nind,int nvar,double timelimit,
+	const char *cmpcr, int fixed,int *pcind,int nind,int nvar,double timelimit,
 	double ntol,bool onlyforward,int *subs,double *subsv,double *bestsv,int *bests,
 	bool printmsg);
+*/
+int callsscma(double* S,double* S2,double* Si,double* Segval,double* Segvct,		
+              double* E,double* Ei,double* Hegvct,double* HegvctTinv,double* HegvctEinv,	
+              int* out,int* vin,int* pcind,int* subs,double* subsv,double* bestsv,int* bests);
+  
 }
 
