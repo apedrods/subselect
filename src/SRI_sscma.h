22f6c29794dd9c44951f5a468920866d6c4babd0
PedroDS <psilva@ucp.pt>
2025-03-16 19:19:36 +0000

    sscma tests 15

--- src/SRI_sscma.h
+++ src/SRI_sscma.h
@@ -23,7 +23,7 @@ int callsscma(double *S,double *S2,double *Si,double *Segval,double *Segvct,
 */
 int callsscma(double* S,double* S2,double* Si,double* Segval,double* Segvct,		
               double* E,double* Ei,double* Hegvct,double* HegvctTinv,double* HegvctEinv,	
-              double wilksval,
+//              double wilksval,
 //              double bartpival,double lawhotval,double ccr12val,int r,
               int kmin,int kmax,int nsol,
               int* out,int* vin,int* pcind,int* subs,double* subsv,double* bestsv,int* bests);
