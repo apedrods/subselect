c2410b4139bc799ac70548cb1a391c7df78c7bd9
PedroDS <psilva@ucp.pt>
2025-03-16 18:52:25 +0000

    sscma tests 12

--- src/SRI_sscma.cpp
+++ src/SRI_sscma.cpp
@@ -21,6 +21,8 @@ SEXP eleaps(SEXP S,SEXP S2,SEXP Si,SEXP Segval,SEXP Segvct,
 	int kmin1 = INTEGER(kmin)[0]; 
 	int klength = kmax1 - kmin1 + 1;
 	int checkcolinearity = INTEGER(onlyforward)[0];  
+	
+	double wilksval1 = REAL(wilksval)[0];
 
 	if (!checkcolinearity) ErrMReals::errmonitreal<double>::dropec = true;   
 	else ErrMReals::errmonitreal<double>::dropec = false;   
