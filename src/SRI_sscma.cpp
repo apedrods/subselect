706172586a70485a9e23dcbd05509e223365c675
PedroDS <psilva@ucp.pt>
2025-03-16 19:06:40 +0000

    sscma tests 14

--- src/SRI_sscma.cpp
+++ src/SRI_sscma.cpp
@@ -53,7 +53,7 @@ SEXP eleaps(SEXP S,SEXP S2,SEXP Si,SEXP Segval,SEXP Segvct,
 	  kmin1,kmax1,nsol1,
     INTEGER(exclude),INTEGER(include),INTEGER(pcindices),
     INTEGER(subsets),REAL(values),REAL(bestvalues),INTEGER(bestsets));
-*/	  
+	  
   int retcode = 0;
 
 	if (retcode == 4) nomemory = true;
