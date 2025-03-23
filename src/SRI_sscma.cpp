4826a7c52c10639cd96a0836186dc50031659ed8
PedroDS <psilva@ucp.pt>
2025-03-23 17:08:09 +0000

    protecting numeric SEXPs 2

--- src/SRI_sscma.cpp
+++ src/SRI_sscma.cpp
@@ -14,17 +14,43 @@ SEXP eleaps(SEXP S,SEXP S2,SEXP Si,SEXP Segval,SEXP Segvct,
 {
 	SEXP subsets,values,bestsets,bestvalues,dimsub,dimval,dimbsets,ans,ans_names;
 
+  Rf_protect(r = AS_INTEGER(r));
+  Rf_protect(kmin = AS_INTEGER(kmin));
+  Rf_protect(kmax = AS_INTEGER(kmax));
+  Rf_protect(nsol = AS_INTEGER(nsol));
+  Rf_protect(nexclude = AS_INTEGER(nexclude));
+  Rf_protect(ninclude = AS_INTEGER(ninclude));
+  Rf_protect(fixed = AS_INTEGER(fixed));
+  Rf_protect(nbindices = AS_INTEGER(nbindices));
+  Rf_protect(dim = AS_INTEGER(dim));
+  Rf_protect(onlyforward = AS_INTEGER(onlyforward));
+  
 	bool optimal,nomemory;
-	int nsol1 = INTEGER(nsol)[0]; 
-	int dim1 = INTEGER(dim)[0]; 
-	int kmax1 = INTEGER(kmax)[0]; 
-	int kmin1 = INTEGER(kmin)[0]; 
+	int r1 = INTEGER_POINTER(r)[0]; 
+	int kmin1 = INTEGER_POINTER(kmin)[0]; 
+	int kmax1 = INTEGER_POINTER(kmax)[0]; 
+	int nsol1 = INTEGER_POINTER(nsol)[0]; 
+	int nexclude1 = INTEGER_POINTER(nexclude)[0]; 
+	int ninclude1 = INTEGER_POINTER(ninclude)[0]; 
+	int fixed1 = INTEGER_POINTER(fixed)[0]; 
+	int nbindices1 = INTEGER_POINTER(nbindices)[0]; 
+	int dim1 = INTEGER_POINTER(dim)[0]; 
 	int klength = kmax1 - kmin1 + 1;
-	int checkcolinearity = INTEGER(onlyforward)[0];  
+	int checkcolinearity = INTEGER_POINTER(onlyforward)[0];  
 	
-//	double wilksval1 = REAL(wilksval)[0];
   Rf_protect(wilksval = AS_NUMERIC(wilksval));
-	double wilksval1 = NUMERIC_VALUE(wilksval);
+	Rf_protect(bartpival = AS_NUMERIC(bartpival));
+	Rf_protect(lawhotval = AS_NUMERIC(lawhotval));
+	Rf_protect(ccr12val = AS_NUMERIC(ccr12val));
+	Rf_protect(timelimit = AS_NUMERIC(timelimit));
+	Rf_protect(maxaperr = AS_NUMERIC(maxaperr));
+	
+	double wilksval1 = NUMERIC_POINTER(wilksval)[0];
+	double bartpival1 = NUMERIC_POINTER(bartpival)[0];
+	double lawhotval1 = NUMERIC_POINTER(lawhotval)[0];
+	double ccr12val1 = NUMERIC_POINTER(ccr12val)[0];
+	double timelimit1 = NUMERIC_POINTER(timelimit)[0];
+	double maxaperr1 = NUMERIC_POINTER(maxaperr)[0];
 	
 	if (!checkcolinearity) ErrMReals::errmonitreal<double>::dropec = true;   
 	else ErrMReals::errmonitreal<double>::dropec = false;   
@@ -50,11 +76,13 @@ SEXP eleaps(SEXP S,SEXP S2,SEXP Si,SEXP Segval,SEXP Segvct,
 	int retcode = extendedleaps::callsscma(
 	  REAL(S),REAL(S2),REAL(Si),REAL(Segval),REAL(Segvct),
 	  REAL(E),REAL(Ei),REAL(Hegvct),REAL(HegvctTinv),REAL(HegvctEinv),
-	  wilksval1,
-//                 *(REAL(bartpival)),*(REAL(lawhotval)),*(REAL(ccr12val)),*(INTEGER(r)),
-	  kmin1,kmax1,nsol1,
-    INTEGER(exclude),INTEGER(include),INTEGER(pcindices),
-    INTEGER(subsets),REAL(values),REAL(bestvalues),INTEGER(bestsets));
+	  wilksval1,bartpival1,lawhotval1,ccr12val1,
+	  r1,kmin1,kmax1,nsol1,
+	  INTEGER(exclude),INTEGER(include),nexclude1,ninclude1,
+    fixed1,INTEGER(pcindices),nbindices1,
+    dim1,timelimit1,maxaperr1,checkcolinearity,
+    INTEGER(subsets),REAL(values),REAL(bestvalues),INTEGER(bestsets),
+    false);
 	  
 //  int retcode = 0;
 
@@ -69,33 +97,34 @@ SEXP eleaps(SEXP S,SEXP S2,SEXP Si,SEXP Segval,SEXP Segvct,
 		Rprintf("of the function argument maxaperr but the numerical accuracy of results may be compromised\n\n");
 	}
 
-   	Rf_protect(dimsub = Rf_allocVector(INTSXP,3));
-   	INTEGER(dimsub)[0] = nsol1;
-   	INTEGER(dimsub)[1] = kmax1;
-    	INTEGER(dimsub)[2] = klength;
-  	SET_DIM(subsets,dimsub); 
+  Rf_protect(dimsub = Rf_allocVector(INTSXP,3));
+	Rf_protect(dimval = Rf_allocVector(INTSXP,2));
+	Rf_protect(dimbsets = Rf_allocVector(INTSXP,2));
+	Rf_protect(ans = NEW_LIST(6));
+	  
+  INTEGER(dimsub)[0] = nsol1;
+  INTEGER(dimsub)[1] = kmax1;
+  INTEGER(dimsub)[2] = klength;
+  SET_DIM(subsets,dimsub); 
 
-   	Rf_protect(dimval = Rf_allocVector(INTSXP,2));
-   	INTEGER(dimval)[0] = nsol1;
-    	INTEGER(dimval)[1] = klength;
-  	SET_DIM(values,dimval); 
+   INTEGER(dimval)[0] = nsol1;
+   INTEGER(dimval)[1] = klength;
+   SET_DIM(values,dimval); 
   	
-   	Rf_protect(dimbsets = Rf_allocVector(INTSXP,2));
-   	INTEGER(dimbsets)[0] = klength;
-    	INTEGER(dimbsets)[1] = kmax1; 
-  	SET_DIM(bestsets,dimbsets); 
+   INTEGER(dimbsets)[0] = klength;
+   INTEGER(dimbsets)[1] = kmax1; 
+   SET_DIM(bestsets,dimbsets); 
 
-  	Rf_protect(ans = NEW_LIST(6));
 
- 	SET_VECTOR_ELT(ans, 0, subsets);
-  	SET_VECTOR_ELT(ans, 1, values);
-  	SET_VECTOR_ELT(ans, 2, bestvalues);
-  	SET_VECTOR_ELT(ans, 3, bestsets);
-  	SET_VECTOR_ELT(ans, 4, Rf_ScalarInteger(optimal));
-  	SET_VECTOR_ELT(ans, 5, Rf_ScalarInteger(nomemory));
+ 	 SET_VECTOR_ELT(ans, 0, subsets);
+   SET_VECTOR_ELT(ans, 1, values);
+   SET_VECTOR_ELT(ans, 2, bestvalues);
+   SET_VECTOR_ELT(ans, 3, bestsets);
+   SET_VECTOR_ELT(ans, 4, Rf_ScalarInteger(optimal));
+   SET_VECTOR_ELT(ans, 5, Rf_ScalarInteger(nomemory));
 
 //	UNPROTECT(8);
-	UNPROTECT(9);
+	UNPROTECT(24);
 	
   	return(ans);
 }
